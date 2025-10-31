ad043d76d23958a85dcb81d05d12aaafb8515bd6
sirajshaikh-dev <ss1425306@gmail.com>
2025-11-01 00:48:52 +0530

    feat: update CORS configuration to allow requests from the production URL

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://leetlab-two.vercel.app/"],
     credentials:true
   })
 )
